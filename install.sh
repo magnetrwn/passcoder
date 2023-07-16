0c0c27e043b7a1346654a4db39fac06a30bdd84e
magnetrwn <rpolli.dev@tutanota.com>
2023-07-16 15:16:05 +0200

    Updated CMakeLists.txt

--- install.sh
+++ install.sh
@@ -6,3 +6,4 @@ cmake ..
 make
 rm -r CMakeFiles cmake_install.cmake CMakeCache.txt Makefile
 printf '\n\u001B[36mDone: Run "./passcoder" from inside the build folder.\u001B[0m\n'
+# TODO: does not install to system yet.
