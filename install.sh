8bf5a71a1cb078ec46eb9e240af09c744e8b5c73
magnetrwn <rpolli.dev@tutanota.com>
2023-07-16 23:36:25 +0200

    Updated CMake, only one list using glob.

--- install.sh
+++ install.sh
@@ -6,8 +6,8 @@ cd build
 cmake ..
 make
 rm -r CMakeFiles cmake_install.cmake CMakeCache.txt Makefile 2> /dev/null
-zip -r "passcoder_build$(date +%s).zip" .
-mv passcoder_build* ../
+#zip -r "passcoder_build$(date +%s).zip" .
+#mv passcoder_build* ../
 
 
 # Run this from project root, it will generate a build zip for you!
