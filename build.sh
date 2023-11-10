48aa7410831e4c670e7dea03de8a9c8190005aab
magnetrwn <rpolli.dev@tutanota.com>
2023-11-10 18:54:48 +0100

    Finishing up project, reorganized build script for compile db.

--- build.sh
+++ build.sh
@@ -5,10 +5,9 @@ cd build
 [ -f passcoder ] && rm passcoder
 cmake ..
 make
+rm ../passcoder_build*.zip 2> /dev/null
 rm -r CMakeFiles cmake_install.cmake CMakeCache.txt Makefile 2> /dev/null
 zip -r "passcoder_build$(date +%s).zip" .
-mv passcoder_build* ../
-
-# Run this from project root, it will generate a build zip for you!
-# TODO: does not install to system yet. Just runs it for now...
+mv passcoder_build*.zip ../
+mv compile_commands.json ../
 ./passcoder
